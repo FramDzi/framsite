c8071af9b0dd5c8967af27413c56ea4be3df4734
Fram <fgkcv25@hotmail.com>
2025-04-02 23:53:14 -0300

    inicial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { base } from "$app/paths";
+</script>
+
+<div class="flex gap-x-4 p-2">
+	<a href="{base}/pagina1">Página 1</a>
+	<a href="{base}/pagina2">Página 2</a>
+</div>
---
+++ src/routes/pagina1/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { base } from "$app/paths";
+</script>
+
+<div class="flex gap-x-4 p-2">
+	<a href="{base}/pagina2">Página 2</a>
+</div>
+
+<a class="m-2" href="/{base}">Voltar</a>
