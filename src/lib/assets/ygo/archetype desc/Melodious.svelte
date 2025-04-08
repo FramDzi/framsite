5d4bd785a768f25ce3e54aeb86745b6c2e708200
Fram <fgkcv25@hotmail.com>
2025-04-07 22:35:31 -0300

    yuzu

--- src/lib/assets/ygo/archetype desc/Melodious.svelte
+++ src/lib/assets/ygo/archetype desc/Melodious.svelte
@@ -1,1 +1,15 @@
-<p>isso é uma decklist pro eijin ficar feliz</p>
+<script>
+    import yuzu from '$lib/assets/ygo/archetype desc/yugioh-arc-v-yu-gi-oh.gif';
+</script>
+
+<div class = "enfeite">
+<p style="margin:10px;">finge que tem uma decklist aqui pro eijin ficar feliz</p>
+<img src = {yuzu} alt = "yuzuu" style="margin: 10px;">
+</div>
+
+<style>
+    .enfeite {
+        background-color: rgba(227, 154, 201, 0.662);
+        border: 3px solid black;
+    }
+</style>
