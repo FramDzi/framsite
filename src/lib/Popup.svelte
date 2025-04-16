fbf566a2f005ec3d01df0b93f3fe331f7b7977b8
Matheus de Azevedo <matheus.marpereira@gmail.com>
2025-04-16 15:08:46 -0300

    Arruma imports e pop-ups pro fram

---
+++ src/lib/Popup.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { base } from '$app/paths';
+
+	let { imageUrl, description } = $props();
+</script>
+
+<div class="enfeite">
+	<p style="margin:10px;">{description}</p>
+	<img src={base + '/gifs/yugioh-arc-v-yu-gi-oh.gif'} style="margin: 10px;" />
+</div>
+
+<style>
+	.enfeite {
+		background-color: rgba(227, 154, 201, 0.662);
+		border: 3px solid black;
+	}
+</style>
